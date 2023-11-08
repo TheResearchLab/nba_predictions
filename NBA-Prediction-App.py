eecdd91e6eb018d85fad072cd13babaa20940c3b
theresearchlab <aarontellis1996@outlook.com>
2023-11-07 21:23:11 -0500

    feat: remove unnecessary dependencies from package imports

--- NBA-Prediction-App.py
+++ NBA-Prediction-App.py
@@ -1,11 +1,10 @@
 import streamlit as st 
-import joblib 
-import requests
-import pandas as pd 
-import numpy as np
+from requests import get
+from pandas import read_json
+from numpy import array
 from sklearn import preprocessing
 from PIL import Image
-import pickle
+from pickle import loads
 
 st.sidebar.success('The Research Lab™')
 
@@ -17,25 +16,25 @@ def main():
                             ('2021-2022 Model','2022-2023 Model'))
 
     url = "https://nba-prediction-api.onrender.com/wl_model?version=%27v1%27" if model_version == '2021-2022 Model' else "https://nba-prediction-api.onrender.com/wl_model?version=%27v2%27"
-    response = requests.get(url)
-    data = pd.read_json(response.json())
-    st.session_state.model = pickle.loads(bytes.fromhex(data['model_object_hex'][0]))
+    response = get(url)
+    data = read_json(response.json())
+    st.session_state.model = loads(bytes.fromhex(data['model_object_hex'][0]))
 
 
     url = "https://nba-prediction-api.onrender.com/upcoming_games"
-    response = requests.get(url)
-    upcoming_games = pd.read_json(response.json(),orient='records')
+    response = get(url)
+    upcoming_games = read_json(response.json(),orient='records')
     upcoming_games['GAME_DATE'] = upcoming_games['GAME_DATE_EST']
 
 
     url = "https://nba-prediction-api.onrender.com/model_features"
-    response = requests.get(url)
-    model_features = pd.read_json(response.json(),orient='records').drop('season',axis=1).dropna()
+    response = get(url)
+    model_features = read_json(response.json(),orient='records').drop('season',axis=1).dropna()
 
 
     url = "https://nba-prediction-api.onrender.com/test_data"
-    response = requests.get(url)
-    test_data = pd.read_json(response.json(),orient='records').dropna().sort_values('GAME_DATE',ascending=True)
+    response = get(url)
+    test_data = read_json(response.json(),orient='records').dropna().sort_values('GAME_DATE',ascending=True)
     test_data = test_data[test_data['SEASON'] == '2022-23'] if model_version == '2021-2022 Model' else test_data[test_data['SEASON'] == '2023-24']
 
 
@@ -43,7 +42,8 @@ def main():
         outcomes = ['AWAY TEAM','HOME TEAM']
         home_features = model_features[model_features['team_id'] == home_id].values[0][3:]
         away_features = model_features[model_features['team_id'] == away_id].values[0][3:]
-        return outcomes[st.session_state.model.predict(np.array([*home_features,*away_features]).reshape(1,14))[0]]
+        return outcomes[st.session_state.model.predict(array([*home_features,*away_features]).reshape(1,14))[0]]
+
         
 
     upcoming_games['MODEL_PREDICTION'] = upcoming_games.apply( lambda row: game_prediction(row['HOME_TEAM_ID'],row['AWAY_TEAM_ID']),axis=1)
@@ -87,10 +87,11 @@ def main():
     st.table(upcoming_games[['GAME_DATE','HOME_TEAM_NAME','AWAY_TEAM_NAME','MODEL_PREDICTION']])
 
 
-try:
-    main()
-except:
-    st.text("404 -  Try Again Later")
-    image = Image.open('404_img.jpg')
-    st.image(image)
+# try:
+#     main()
+# except:
+#     st.text("404 -  Try Again Later")
+#     image = Image.open('404_img.jpg')
+#     st.image(image)
 
+main()
